b625b55ebfb48cd8cb5aaeefc02e84738711c32e
Himani Kapadia <kapadiahimani6@gmail.com>
2026-07-05 23:36:30 +0530

    Update secure_atm.py
    Added features:
    1. validation of PIN
    2. Deposit validation and function
    3. Withdrawal validation and function
    4. Displayed final bank balance hiding private attribute's data

--- 02-Encapsulation/secure_atm.py
+++ 02-Encapsulation/secure_atm.py
@@ -6,6 +6,34 @@ class Atm:
         self.__pin=pin
         self.__initial_balance=initial_balance
 
+    def display_balance(self,entered_pin):
+        if entered_pin==self.__pin:
+            print(f"\nAccount holder: {self.acc_name}")
+            print(f"Bank Name: {self.bank_name}")
+            print(f"Current Balance: {self.__initial_balance}")
+        else:
+            print("\n\nInvalid Pin! Access denied")
+    def deposit(self,entered_pin,amt):
+        if entered_pin==self.__pin:
+            if amt>0:
+                self.__initial_balance+=amt
+                print(f"\n{amt} Rs. deposited successfully in {self.bank_name}")
+            else:
+                print("\nInvalid deposit Amount!")
+        else:
+            print("\nInvalid Pin! deposit blocked!")
+    def withdraw(self,entered_pin,amt):
+        if entered_pin==self.__pin:
+            if amt>self.__initial_balance:
+                print(f"\nInsuffient balance! you can't withdraw")
+            elif amt<=0:
+                print("\nInvalid withdrawal amount")
+            else:
+                self.__initial_balance-=amt
+                print(f"\n{amt} Rs. successfully withdrawn!")
+        else:
+            print("\nInvalid pin! withdrawal blocked!")
+
 acc1=Atm("Dr. Raj",1234,2000) #Object created
 
 #we can access class and public attributes
@@ -14,5 +42,25 @@ print(f"Account holder name (Public attribute): {acc1.acc_name}")
 
 #Demonstration of Encapsulation
 # print(acc1.__initial_balance) --> This will create an error 
-print("\n The PIN and Balance attributes are safely encapsulated and hidden!")
+print("The PIN and Balance attributes are safely encapsulated and hidden!")
+
+#Test 1: Try checking balance with the WRONG pin
+acc1.display_balance(1111)
+
+#Test 2: Deposit money with the CORRECT pin
+acc1.deposit(entered_pin=1234,amt=2000)
+
+#ERROR due to accessing private attributes
+#print(acc1.__initial_balance()) 
+
+# Test 3: Attempt a withdrawal with INSUFFICIENT funds
+acc1.withdraw(entered_pin=1234,amt=1000000)
+
+# Test 4: Successful withdrawal
+acc1.withdraw(entered_pin=1234,amt=500)
+
+# Test 5: Display final statement
+acc1.display_balance(entered_pin=1234)
+
+
         

8d46a2b64a64b487164dbab20f6ed2d9ed16a9c5
himanikapadia <kapadiahimani6@gmail.com>
2026-07-14 23:04:23 +0530

    Demonstrates the Composition principle through a 'has-a' relationship where the Laptop class contains independent component objects that work together

---
+++ 07-Composition/smart_laptop.py
@@ -0,0 +1,29 @@
+class CPU:
+    #Composition method of CPU class
+    def process(self):
+        print("CPU is processing Data...")
+
+class RAM:
+    def load(self):
+        print("RAM is Loading Applications....")
+
+class Battery:
+    def power(self):
+        print("Battery is Supplying Power....")
+
+class laptop:
+    def __init__(self):
+        self.cpu=CPU() #Laptop HAS a CPU
+        self.ram=RAM() #Laptop HAS a RAM
+        self.battery=Battery() #Laptop HAS a Battery
+
+    def start(self):
+        print("==== Starting Laptop =====")
+        self.battery.power()
+        self.cpu.process()
+        self.ram.load()
+        print("Laptop is ready to use...")
+
+#Drivers Code
+lap=laptop()
+lap.start()
